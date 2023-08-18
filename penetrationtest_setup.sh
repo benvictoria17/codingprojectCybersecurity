e9ddc407746e7d368a4e7f42cf9b8547c73ed413
Ben Victoria <57550716+benvictoria17@users.noreply.github.com>
2023-08-18 14:20:12 -0700

    Update penetrationtest_setup.sh

--- penetrationtest_setup.sh
+++ penetrationtest_setup.sh
@@ -1,7 +1,7 @@
 # Add kali linux sourc
 sudo add-apt-repository  deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware'
 sudo add-apt-repository  deb https://deb.parrot.sh/parrot/ rolling main contrib non-free'
-deb-src https://deb.parrot.sh/parrot/ rolling main contrib non-free'
+sudo add-apt-repository  deb-src https://deb.parrot.sh/parrot/ rolling main contrib non-free'
 sudo add-apt-repository 'deb https://deb.parrot.sh/parrot/ rolling-security main contrib non-free'
 sudo add-apt-repository 'deb-src https://deb.parrot.sh/parrot/ rolling-security main contrib non-free'
 sudo add-apt-repository 'deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse'
